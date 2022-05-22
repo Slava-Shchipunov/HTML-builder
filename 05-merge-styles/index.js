f596ea3d4a201fb2a3caf43822a018a187fa10ac
Slava-Shchipunov <shchipunovsd@gmail.com>
2022-05-22 23:13:08 +0700

    feat: solve 05-merge-styles task

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+const { readdir } = require('fs/promises');
+
+const fs = require('fs');
+const fsPromises = fs.promises;
+
+async function mergeStyles(folderPath) {
+  try {
+    const files = await readdir(folderPath, {withFileTypes: true});
+
+    for (const file of files) {
+      if (file.isFile() && path.extname(file.name) === '.css') {
+        let streem = fs.createReadStream(path.resolve(__dirname, 'styles/' + file.name));
+
+        streem.on('data', (data) => {
+          fsPromises.appendFile(path.resolve(__dirname, 'project-dist/' + 'bundle.css'), data + '\n');
+        });
+      }
+    }
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+fs.rm(path.resolve(__dirname, 'project-dist/' + 'bundle.css'), {recursive: true, force: true}, (error) => {
+  if (error) {
+    console.error(error.message);
+    return;
+  }
+  
+  mergeStyles(path.resolve(__dirname, 'styles'));
+});
